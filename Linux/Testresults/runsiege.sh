e09d119d0d25ae503140fb02c6ab1b270be4d704
lilswif <dready701@hotmail.com>
2015-11-06 17:03:33 +0100

    siege script up&running

--- Linux/Testresults/runsiege.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-siege -c10 -d10 -t5M
-siege -c20 -d10 -t5M
-siege -c40 -d10 -t5M
-siege -c80 -d10 -t5M
-siege -c160 -d10 -t5M
---
+++ Linux/scripts/runsiege.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+function echocolor() { # $1 = string
+    COLOR='\033[1;33m'
+    NC='\033[0m'
+    printf "${COLOR}$1${NC}\n"
+}
+
+
+echocolor "**********************"
+echocolor "**  SIEGE TEST RUN  **"
+echocolor "**********************"
+echo 
+echocolor "how many times would you like to run a test: "
+read AANTALXTEST
+echocolor "How long you want the test to run? (format: Number + S/M/H/D)"
+read Twaarde
+echocolor "Delay-value?: "
+read Dwaarde
+echocolor "How many concurring Users?"
+read Cwaarde
+echocolor "Name of the logfile"
+read lognaam
+echocolor "Clear originial logfile after test? (y / n)"
+read clear
+echo 
+echo
+echocolor "***********************"
+echocolor "** PREPARING ENGAGE  **"
+echocolor "***********************"
+echo
+echo gonna run:
+echo siege -c$Cwaarde -d$Dwaarde -t$Twaarde
+echo x $AANTALXTEST "times"
+echo
+echocolor "***********************"
+echocolor "**      ENGAGING     **"
+echocolor "***********************"
+
+COUNTER=0
+while [  $COUNTER -lt $AANTALXTEST ]; do
+siege -c $Cwaarde -d $Dwaarde -t $Twaarde
+let COUNTER=COUNTER+1 
+ done
+echo 
+echo
+echocolor "***********************"
+echocolor "**   DONE ATTACKING  **"
+echocolor "***********************"
+echo
+echo
+echocolor "***********************"
+echocolor "**MAKING CSV IN HOME **"
+echocolor "***********************"
+echo
+echo Gonna run:
+echo cp -r /usr/local/var/siege.log /home/fred/$lognaam.CSV
+cp -r /usr/local/var/siege.log /home/fred/$lognaam.CSV
+echo
+echo
+            if [ $clear = "y" ]; then
+            	echo
+				echocolor "***********************"
+				echocolor "**Clearing log file  **"
+				echocolor "***********************"
+				echo
+            	sudo bash -c 'echo "" > /usr/local/var/siege.log'
+            else
+                           	echo
+				echocolor "*****************************"
+				echocolor "**  NOT Clearing log file  **"
+				echocolor "*****************************"
+				echo
+            fi
+
+
+echocolor "***********************"
+echocolor "**    DONE, HOORAY!  **"
+echocolor "***********************"
